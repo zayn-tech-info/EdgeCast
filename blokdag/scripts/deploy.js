0a0297d20cac8826d00fa627c789ba5c8e9a466a
zayn-tech-info <zayntech.info@gmail.com>
2025-09-26 18:18:27 +0100

    feat: implement PredictionMarket contract and associated deployment scripts; add API for pool management

--- blokdag/scripts/deploy.js
+++ blokdag/scripts/deploy.js
@@ -1,11 +1,15 @@
-export default async function main(hre) {
-  console.log("Starting deployment...");
-  const PredictionGame = await hre.ethers.getContractFactory("PredictionGame");
-  const game = await PredictionGame.deploy(
-    hre.ethers.parseEther("0.01"), // Entry fee
-    86400 // Duration: 24 hours
-  );
+const hre = require("hardhat");
 
-  await game.waitForDeployment();
-  console.log(`PredictionGame deployed to: ${game.target}`);
+async function main() {
+  const PredictionMarket = await hre.ethers.getContractFactory(
+    "PredictionMarket"
+  );
+  const pm = await PredictionMarket.deploy();
+  await pm.deployed();
+  console.log("PredictionMarket deployed to:", pm.address);
 }
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
