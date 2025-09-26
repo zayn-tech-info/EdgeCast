f55707826fa03b41fd14552ebe0e33671be617cd
youngemeritus <sobalajetomiwa@gmail.com>
2025-09-26 09:37:35 -0700

    Update hardhat config for backend integration

--- blokdag/hardhat.config.ts
+++ blokdag/hardhat.config.ts
@@ -36,6 +36,12 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    blockdag: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("BLOCKDAG_RPC_URL"),
+      accounts: [configVariable("BLOCKDAG_PRIVATE_KEY")],
+    },
   },
 };
 
