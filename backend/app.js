b38bd895e18afff506a4057dc69e35bbc003842a
zayn-tech-info <zayntech.info@gmail.com>
2025-09-26 12:35:02 +0100

    feat: implement authentication with nonce verification and JWT
    - Added auth controller with nonce generation, signature verification, and protected route.
    - Created user model with validation for user registration.
    - Established MongoDB connection in a separate module.
    - Updated package.json and package-lock.json to include new dependencies (body-parser, cors, ethers, jsonwebtoken, validator).
    - Added .gitignore file to exclude unnecessary files from version control.
    - Refactored server.js to load environment variables and start the server.
    - Created auth routes for nonce generation and verification.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const connectTODB = require("./lib/db");
+
+const authRouter = require("./routes/auth.route");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth/", authRouter);
+connectTODB();
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv");
 
-const app = require("./app");
-
 dotenv.config({ path: ".env.local" });
+
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
